ee0b7ce964296df1e5669a27f69f0eb1503cf07b
HeejuKo <khjg00gle96.gmail>
2024-10-10 00:43:48 +0900

    fix_url

--- domi/urls.py
+++ domi/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
