4dae384870a2f1785fafff45b3e20b590c9d8d01
HeejuKo <khjg00gle96.gmail>
2024-10-09 22:36:59 +0900

    fix

--- domi/settings.py
+++ domi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['secret_key']
+SECRET_KEY = os.environ['SECRET_KEY']
 #SECRET_KEY = 'j)710&r(s@+d=mq=1%so5c)h^-dt60ve7#u=&f**2+&z^3sgrb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
